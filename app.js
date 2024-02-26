c136ef743408e2608b833598a9ae333070583d63
dharmik <dharmikvalani57@gmail.com>
2024-02-26 14:39:34 +0530

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import cors from 'cors';
 import { db } from "./config/dbConfig.js";
 import { courseRouter } from "./router/course.router.js";
 
@@ -9,6 +10,8 @@ db;
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
 
 app.use("/api/courses", courseRouter);
 app.listen(PORT, () => {
